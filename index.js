9596bd7e024fbc59e0eed58f2808afb0da0bbbb0
Cristiano Maynez <cethereum@protonmail.com>
2022-10-04 01:43:27 +0800

    program lists names

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const axios = require('axios');
 
+axios.get('https://swapi.dev/api/people/')
+    .then(response => {
+        response.data.results.forEach(element => {
+            console.log(element.name);
+        });
+    })
